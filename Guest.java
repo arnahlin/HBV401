f6ec75a5cd28bf346bbd516b278d56e738da996a
thh167 <32545064+thh167@users.noreply.github.com>
2020-03-23 11:42:40 +0000

    Add existing file

---
+++ Guest.java
@@ -0,0 +1,34 @@
+
+// H�fundur: Þurí
+//
+// Notkun: java -cp .;sqlite-jdbc-....jar Guest
+// Fyrir:  SQLite skrain HotelDB.db inniheldur Hotel gagnagrunninn.
+// Eftir:  B�i� er a� gefa �llum starfsm�nnum 10% dala launah�kkun.
+
+import java.sql.*;
+
+public class Guest
+{
+	public static void main( String[] args )
+		throws Exception
+	{
+		Connection conn = null;
+		Statement stmt = null;
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:HotelDB.db");
+			stmt = conn.createStatement();
+			stmt.executeUpdate("INSERT INTO Guest VALUES('Gunnar Atlason','2207896679')");
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			//if( stmt!=null ) stmt.close();
+			if( conn!=null ) conn.close();
+		}
+	}
+}
