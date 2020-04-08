5081fd45d65b3b7e058b15c1967be8d77de395c0
thh167 <32545064+thh167@users.noreply.github.com>
2020-04-08 10:31:10 +0000

    Controllerar 3 með tengingu og nýjar töflur

--- CreateTables.sql
+++ CreateTables.sql
@@ -13,29 +13,30 @@ rooms INT
 
 CREATE TABLE IF NOT EXISTS Room( 
 roomID INT,
-hotelNr INT REFERENCES Hotel(hotelID), 
+hotelID INT, 
 price INT, 
 booked char(1),
 available char(1),
-PRIMARY KEY(roomID, hotelNr),
-FOREIGN KEY(hotelNr) REFERENCES HOTEL(hotelID)
+PRIMARY KEY(roomID, hotelID),
+FOREIGN KEY(hotelID) REFERENCES HOTEL(hotelID)
 );
 
 CREATE TABLE IF NOT EXISTS Reservation( 
 reservationID INT,
 guestName VARCHAR(100) REFERENCES Guest(name),
-occupancy DATE,
+checkIn DATE,
+checkOut DATE,
 roomID INT,
 PRIMARY KEY(reservationID),
 FOREIGN KEY(roomID) REFERENCES Room(roomID)
 );
 
-CREATE TABLE IF NOT EXISTS Occupancy( 
-checkIn DATE,
-checkOut DATE
-);
-
 .mode column
 .headers on
 .tables
+
+SELECT * FROM Guest;
+SELECT * FROM Hotel;
+SELECT * FROM Room;
+SELECT * FROM Reservation;
 
