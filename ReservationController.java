be1c6330c29e6d412d4c4938a7c35c1c41ea8ca6
thh167 <32545064+thh167@users.noreply.github.com>
2020-04-09 20:12:00 +0000

    Setja inn nýjan guest og Reservation

--- ReservationController.java
+++ ReservationController.java
@@ -1,47 +1,87 @@
-// javac reservationController.java
+// javac ReservationController.java
 // java -cp .:sqlite-jdbc-3.18.0.jar ReservationController
 // SQLite skrain HotelDB.db inniheldur Hotel gagnagrunninn.
+// .read CreateTables.sql til að sjá hvort breytingin hafi komist inn.
 
 import java.sql.*;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
 
 public class ReservationController {
+	Guest nyrGestur;
 
-	Connection conn = null; //
-	Statement stmt = null; // Statement: The object used for executing a static SQL statement and returning
-							// the results it produces.
-
-	/**
-	 * Marks the room as booked
-	 */
-	public void bookRoom(int roomId) {
-
-	}
-
-	public static void main(String[] args) throws Exception {
-
-		try {
+	public void newGuest(String name, String kennitala, String reservationID) throws Exception {
+		Connection conn = null; 
+		try
+		{
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection("jdbc:sqlite:HotelDB.db");
-			stmt = conn.createStatement();
-			// stmt.executeUpdate("INSERT INTO Guest VALUES('Jón
-			// Gunnarsson','2309841329','38'),('Sigurður Jónasson','0109725469','43')");
-			// stmt.executeUpdate("INSERT INTO Reservation VALUES('12','Sigurður
-			// Jónasson','2020-06-20','057');");
-			// stmt.executeUpdate("INSERT INTO Occupancy
-			// VALUES('2020-06-30','2020-07-04','022');");
+			String newGuest = "INSERT INTO Guest VALUES('"+name +"','"+kennitala+"','" +reservationID+"')"; //Setting a new Guest.
+			PreparedStatement pstmt1 = conn.prepareStatement(newGuest);
+			pstmt1.executeUpdate(); 
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if( conn!=null ) conn.close();
+		}
+	}
+	
+	// Marks the room as booked
+	public void bookRoom(int roomID) throws Exception {
+		
+		Connection conn = null; //
+		PreparedStatement pstmt = null;
+		//ResultSet r = null;
 
-			/*
-			 * Ath, ég commentaði út þessar 3 línur því ég er búin að setja þær inn í
-			 * gagnagrunnana til að sjá hvort að tengingin virki. Ef maðu keyrir skjalið
-			 * aftur með þessum gildum þá þá kemur villa út af primary key. Þ.e maður þarf
-			 * að setja inn ný gildi.
-			 */
+		try { // athuga hvort herbergi sé laust
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:HotelDB.db");
+			String avail = "SELECT roomID, available FROM Room WHERE available='y' AND roomID=789 AND hotelID=18";
+			pstmt = conn.prepareStatement(avail);
+			ResultSet rs = pstmt.executeQuery();
 
+			if (!rs.next() ) {    
+				System.out.println("This room is occupied");
+			   }
+			   else{ // setja í Reservation töflu 
+				String setReservation = "INSERT INTO Reservation VALUES('12','John Johnsson','2020-09-09','2020-09-21','220')"; //setting room in room table to be occupied
+				PreparedStatement pstmt1 = conn.prepareStatement(setReservation);
+				pstmt1.executeUpdate(); 
+				// updeita herbergi í Room yfir í available = n
+				String setBooked = "UPDATE Room set available='y' WHERE roomID=789 AND hotelID=18"; //setting room in room table to be occupied
+				PreparedStatement pstmt2 = conn.prepareStatement(setBooked);
+				pstmt2.executeUpdate();
+			}    
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if (conn != null)
 				conn.close();
 		}
+
+	}
+
+	public static void main(String[] args) throws Exception  {
+		ReservationController test = new ReservationController();
+		Guest nyrGestur = new Guest();
+		StdOut.print("Enter full name and ID number ");
+		String nafn = "";
+		String IDnumber = "";
+
+			nafn = StdIn.readLine();
+			nyrGestur.setName(nafn);
+			IDnumber = StdIn.readLine();
+			nyrGestur.setId(IDnumber);
+
+		System.out.println("Nafn: " +nyrGestur.name);
+		System.out.println("Kennitala: " +nyrGestur.kennitala);
+
+		String resID = "00";
+		test.newGuest(nyrGestur.name, nyrGestur.kennitala, resID);
+
 	}
 }
