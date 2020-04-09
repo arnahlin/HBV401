8d5aa09651cd15fa9092da127288caf492d6f14a
arnahlin <arnahlin@github.com>
2020-04-09 18:52:22 +0000

    work on hotel

--- Hotel.java
+++ Hotel.java
@@ -1,24 +1,54 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Hotel {
+    private int hotelID;
     private String hotelName;
     private String location;
-    private List rooms; // maybe a list of roomId's?
+    private List<Integer> rooms; // maybe a list of roomId's?
+   
 
-    public Hotel(String hotelName, String location) { // variable rooms is missing
+    // variable rooms is missing from this method, not sure how to do this
+    // rooms is set as an int in the hotel table in database
+    public Hotel(String hotelName, String location) { 
         this.hotelName = hotelName;
         this.location = location;
     }
 
+    public int getHotelID() {
+        return hotelID;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setHotelID(int id) {
+        this.id = hotelID;
+    } 
+
+    public void setHotelName(String name) {
+        this.name = hotelName;
+    }
+
+    public void setLocation(String loc) {
+        this.loc = location;
+    }
+
     public void addRoom(int roomID, int type, int price) { // enum type
-
+        String sql = "INSERT INTO Room VALUES(" + roomID + hotelID + price + available + ");";
     }
-
+  
     public void removeRoom(int roomID) {
-
+        String sql = "DELETE FROM Room WHERE roomID = " + roomID + ";";
     }
 
-    public List searchByLocation(String location) {
-        
+    public void updateAvailability(int roomID, char a) {
+        String sql = "UPDATE Room SET available = "+ a + "WHERE roomID = " + roomID + ";";
     }
+
 }
