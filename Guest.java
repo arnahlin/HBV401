98f5fc0bdcabbc51d751766946fafc228d2e5935
thh167 <32545064+thh167@users.noreply.github.com>
2020-04-09 20:08:35 +0000

    Setja inn nýjan notanda og reservation

--- Guest.java
+++ Guest.java
@@ -1,36 +1,56 @@
-
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
 /**
  * Hotel booking Guest.
  * 
  */
 public class Guest {
-	private int kennitala;
-	private String name;
-	private int reservationID;
+	public String kennitala;
+	public String name;
+	public int reservationID;
 
-	public Guest(final int kennitala, final String name, int reservationID) {
+	public Guest(String kennitala, String name, int reservationID) {
         this.kennitala = kennitala;
         this.name = name;
         this.reservationID = reservationID;
 	}
+
+	public Guest() {
+
+	}
 	
-	public int getKennitala(){
+	public String getKennitala(){
 		return kennitala;
 	}
 
-	public void setId(int kennitala){
+	public void setId(String kennitala){
 		this.kennitala = kennitala;
+	//	StdOut.println(kennitala);
 	}
 
 	public void setName(String name) {
-        this.name = name;
+		this.name = name;
+	//	StdOut.println(name);
 	}
 	
-	public string getName() {
+	public String getName() {
         return name;
     }
 	
+	public static void main(String[] args)  {
+			Guest gestur = new Guest();
+			StdOut.print("Enter full name and ID number ");
+			String nafn = "";
+			String IDnumber = "";
 
+				nafn = StdIn.readLine();
+				gestur.setName(nafn);
+				IDnumber = StdIn.readLine();
+				gestur.setId(IDnumber);
+
+			System.out.println("Nafn: " +gestur.name);
+			System.out.println("Kennitala: " +gestur.kennitala);
+		}
 
 
 
--- ReservationController.java
+++ ReservationController.java
@@ -4,42 +4,84 @@
 // .read CreateTables.sql til að sjá hvort breytingin hafi komist inn.
 
 import java.sql.*;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
 
 public class ReservationController {
+	Guest nyrGestur;
 
-	// Marks the room as booked
-	public void bookRoom(int roomId) {
-
+	public void newGuest(String name, String kennitala, String reservationID) throws Exception {
+		Connection conn = null; 
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:HotelDB.db");
+			String newGuest = "INSERT INTO Guest VALUES('"+name +"','"+kennitala+"','" +reservationID+"')"; //Setting a new Guest.
+			PreparedStatement pstmt1 = conn.prepareStatement(newGuest);
+			pstmt1.executeUpdate(); 
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if( conn!=null ) conn.close();
+		}
 	}
-
-	public static void main(String[] args) throws Exception {
+	
+	// Marks the room as booked
+	public void bookRoom(int roomID) throws Exception {
+		
 		Connection conn = null; //
 		PreparedStatement pstmt = null;
 		//ResultSet r = null;
 
-		try {
+		try { // athuga hvort herbergi sé laust
 			Class.forName("org.sqlite.JDBC");
 			conn = DriverManager.getConnection("jdbc:sqlite:HotelDB.db");
-			String avail = "SELECT roomID, hotelID FROM Room WHERE roomID=789 AND hotelID=18";
+			String avail = "SELECT roomID, available FROM Room WHERE available='y' AND roomID=789 AND hotelID=18";
 			pstmt = conn.prepareStatement(avail);
 			ResultSet rs = pstmt.executeQuery();
 
 			if (!rs.next() ) {    
 				System.out.println("This room is occupied");
 			   }
-			   else{
-				String setBooked = "UPDATE Room set available='n' WHERE roomID=789 AND hotelID=18"; //setting room in room table to be occupied
+			   else{ // setja í Reservation töflu 
+				String setReservation = "INSERT INTO Reservation VALUES('12','John Johnsson','2020-09-09','2020-09-21','220')"; //setting room in room table to be occupied
+				PreparedStatement pstmt1 = conn.prepareStatement(setReservation);
+				pstmt1.executeUpdate(); 
+				// updeita herbergi í Room yfir í available = n
+				String setBooked = "UPDATE Room set available='y' WHERE roomID=789 AND hotelID=18"; //setting room in room table to be occupied
 				PreparedStatement pstmt2 = conn.prepareStatement(setBooked);
 				pstmt2.executeUpdate();
-
 			}    
-
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if (conn != null)
 				conn.close();
 		}
+
+	}
+
+	public static void main(String[] args) throws Exception  {
+		ReservationController test = new ReservationController();
+		Guest nyrGestur = new Guest();
+		StdOut.print("Enter full name and ID number ");
+		String nafn = "";
+		String IDnumber = "";
+
+			nafn = StdIn.readLine();
+			nyrGestur.setName(nafn);
+			IDnumber = StdIn.readLine();
+			nyrGestur.setId(IDnumber);
+
+		System.out.println("Nafn: " +nyrGestur.name);
+		System.out.println("Kennitala: " +nyrGestur.kennitala);
+
+		String resID = "00";
+		test.newGuest(nyrGestur.name, nyrGestur.kennitala, resID);
+
 	}
 }
