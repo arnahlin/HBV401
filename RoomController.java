5183a335ed1018cd51446c8f514da745ce4b31a5
gudb <gbs26@hi.is>
2020-04-10 17:30:39 +0000

    .

--- RoomController.java
+++ RoomController.java
@@ -3,18 +3,26 @@
 // SQLite skrain HotelDB.db inniheldur Hotel gagnagrunninn.
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoomController {
-    private List<Room> rooms; // ætti að vera tengt við Room eh veginn
-    
 
-    /**
-     * Searhces the database for a room that meets
-     * the search criteria.
-     * @return Room object
-     */
-    public Room search(String type) {
+	private Room room = new Room();
+
+    private Connection dataBase() {
+        Connection conn = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:HotelDB.db");
+		}
+		catch( Exception e ) {
+			e.printStackTrace();
+        }
+        return conn;
+	}
+
+    public ArrayList<Room> search(String type) {
 		ArrayList<Room> temp = new ArrayList<Room>();
 		
 		//todo
@@ -23,33 +31,20 @@ public class RoomController {
     }
 
     /**
-     * Marks the room as booked 
-     */
-    public void bookRoom(int roomId) {
+	 * Marks the room as booked
+	 * 
+	 * @throws SQLException
+	 */
+	public void bookRoom(int roomID) throws SQLException {
+		Connection conn = dataBase();
+		Statement stmt = conn.createStatement();
 		String s = "UPDATE Room SET available = " + "n " + "WHERE roomID = " + roomID + ";";
-    }
+		ResultSet rs = stmt.executeQuery(s);
+	}
+	
 
     public static void main( String[] args ) throws Exception {
-		Connection conn = null;
-		Statement stmt = null;  
-		try
-		{
-			Class.forName("org.sqlite.JDBC");
-			conn = DriverManager.getConnection("jdbc:sqlite:HotelDB.db");
-			stmt = conn.createStatement();
-		// 	stmt.executeUpdate("INSERT INTO Room VALUES('220','12','13900','y','n')");
-		
-        /* Ath, ég commentaði línuna hér að ofan út, því ef á að keyra aftur þarf að setja inn ný gildi því annars kemur villa út af primary key :) */
 
-		}
-		catch( Exception e )
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			if( conn!=null ) conn.close();
-		}
 	}
 
 }
