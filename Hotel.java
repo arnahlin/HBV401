720914c662f7f44e4ae5e9f74a2902c304bbf0f1
thh167 <32545064+thh167@users.noreply.github.com>
2020-04-13 13:45:28 +0000

    .

--- Hotel.java
+++ Hotel.java
@@ -1,24 +1,54 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Hotel {
+    private int hotelID;
     private String hotelName;
     private String location;
-    private List rooms; // maybe a list of roomId's?
-
-    public Hotel(String hotelName, String location) { // variable rooms is missing
+    // private List<Integer> rooms; // maybe a list of roomId's?
+    private int rooms;
+   
+    public Hotel(int hotelID, String hotelName, String location, int rooms) { 
+        this.hotelID = hotelID;
         this.hotelName = hotelName;
+        this.location = location;
+        this.rooms = rooms;
+    }
+
+    public int getHotelID() {
+        return hotelID;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setHotelID(int hotelID) {
+        this.hotelID = hotelID;
+    } 
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public void setLocation(String location) {
         this.location = location;
     }
 
-    public void addRoom(int roomID, int type, int price) { // enum type
-
+    public void addRoom(int roomID, int hotelID, int price, char available) {
+        String sql = "INSERT INTO Room VALUES(" + roomID + hotelID + price + available + ");";
     }
-
+  
     public void removeRoom(int roomID) {
-
+        String sql = "DELETE FROM Room WHERE roomID = " + roomID + ";";
     }
 
-    public List searchByLocation(String location) {
-        
+    public void updateAvailability(int roomID, char a) {
+        String sql = "UPDATE Room SET available = "+ a + "WHERE roomID = " + roomID + ";";
     }
+
 }
--- Room.java
+++ Room.java
@@ -3,21 +3,72 @@ import java.util.List;
 public class Room {
     private int roomID;
     private int price;
-    private boolean booked; 
+    private int hotelID;
+    private boolean booked;
     private List reserved; // List of reserved rooms
-    private List available; // List of available rooms
+    private String available; // List of available rooms
 
-    private enum type {
-        small,
-        medium, 
-        large
+    public Room() {
+
     }
 
-    /** 
-     * Creates the reservation for the room?
-     */
-    public void makeReservation() {
+    public int getRoomID(){
+        return roomID;
+    }
 
+    public void setRoomID(int roomID){
+        this.roomID = roomID;
+    }
+
+    public int getHotelID() {
+        return hotelID;
+    }
+
+    public void setHotelID(int hotelID) {
+        this.hotelID = hotelID;
+    }
+
+    public String getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(final String available) {
+        this.available = available;
+    }
+
+    public Room(final int roomID) {
+        this.roomID = roomID;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(final int price) {
+        this.price = price;
+    }
+
+    public boolean getBooked() {
+        return booked;
+    }
+
+    public void setBooked(final boolean booked) {
+        this.booked = booked;
+    }
+
+	public String toString()
+	{
+		return "[Room id=" + this.roomID +
+				", hotel=" + this.hotelID + 
+				", price=" + this.getPrice() +
+				", available=" + this.getAvailable() + "]";
+	}
+    /**
+     * Creates the reservation for the room? Is this supposed to be here or in the
+     * ReservationController?
+     */
+    public void makeReservation(final int roomID, final int price, final boolean booked) {
+        final String s = "INSERT INTO Reservation VALUES(" + roomID + price + booked + ");";
     }
 
 }
